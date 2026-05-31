8c0bf081508ccb881628406949e19778c90689ad
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-05-31 21:01:02 +0900

    [level 1] Title: 시저 암호, Time: 0.10 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+        for (auto c : s)
+        {
+            if (c >= 'a' && c<= 'z')
+            {
+                c = toupper(c);
+                c = c + n;
+                if (c > 'Z') c = c - 26;
+                c = tolower(c);
+
+                string tmp = "";
+                tmp += c;
+                answer += tmp;
+            }
+
+            else if (c >= 'A' && c <= 'Z')
+            {
+                c = c + n;
+                if (c > 'Z') c = c - 26;
+
+                string tmp = "";
+                tmp += c;
+                answer += tmp;
+            }
+            else if (c = ' ')
+            {
+                answer += " ";
+            }
+        }
+    
+    return answer;
+}
