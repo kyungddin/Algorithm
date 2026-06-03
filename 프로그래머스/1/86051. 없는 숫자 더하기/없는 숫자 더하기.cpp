fc9283ec6bae84e0722b1ede5a32f3a8f4b1657a
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-06-03 16:47:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -1,31 +1,12 @@
-#include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int answer = 0;
+    int answer = 45;
     
-    sort(numbers.begin(), numbers.end(), greater<>());
-    
-    int i = 0;
-    while(true)
-    {
-        if( numbers.back() != i) 
-        {
-            answer += i;
-            i++;
-        }
-        else
-        {
-            numbers.pop_back();
-            i++;
-        }
-        
-        if (i == 10) break;
-        
-    }
+    for (auto num : numbers)
+        answer -= num;
     
     return answer;
 }
