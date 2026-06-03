159fe9bd16fc069a9d4c2600318e8245d766d911
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-06-03 17:25:23 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.02 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -1,105 +1,73 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 string solution(string new_id) {
-    string tmp1 = "";
-    string tmp2 = "";
-    string tmp3 = "";
-    string tmp4 = "";
-    string tmp5 = "";
-    string tmp6 = "";
-    string tmp7 = "";
+    string buffer;
+    string result;
     
     // Step 1: 소문자로 치환
-    for (auto c : new_id)
+    for (auto ch : new_id)
     {
-        if( c >= 'A' && c <= 'Z')
-            c = tolower(c);
-        tmp1 += c;
+        if( ch >= 'A' && ch <= 'Z')
+            ch = tolower(ch);
+        
+        buffer += ch;
     }
+    result = buffer;
     
+
     // Step 2: 문자 제거
-    for (auto c : tmp1)
+    buffer = "";
+
+    for (auto c : result)
     {
-        if (c >= 'a' && c <= 'z')
-            tmp2 += c;
-        else if(c >= '0' && c <= '9')
-            tmp2 += c;
-        else if (c == '-')
-            tmp2 += c;
-        else if (c == '_')
-            tmp2 += c;
-        else if (c == '.')
-            tmp2 += c;
+        if (isalpha(c) || isdigit(c) || c == '-' || c == '_' || c == '.') 
+            buffer += c;
         else
             continue;
     }
-    
+    result = buffer;
+
     // Step 3: . 압축
-    
-    for (int i=0; i<tmp2.size(); i++)
+    buffer = "";
+    int size = result.size();
+
+    for (int i = 0; i < size; )
     {
-        if(tmp2[i] == '.')
-        {
-            if (i == tmp2.size()-1)
-            {
-                tmp3 += tmp2[i];
-            }
-            else if(tmp2[i+1] == '.')
-                continue;
-            else
-            {
-                tmp3 += tmp2[i];
-            }
-        }
-        else 
-        {
-            tmp3 += tmp2[i];
-        } 
+        buffer += result[i];
+        if (result[i++] == '.')
+            while (result[i] == '.') i++;
     }
+    
+    result = buffer;
     
     // Step 4 : 시작과 끝의 마침표 제거
-    for (int i=0; i<tmp3.size(); i++)
-    {
-        if (i == 0 && tmp3[i] == '.') continue;
-        else if (i == tmp3.size()-1 && tmp3[i] == '.') continue;
-        else tmp4 += tmp3[i];
-    }
+    if (result.front() == '.') 
+        result.erase(result.begin());
+    if (result.back() == '.') 
+        result.erase(result.end() - 1);
     
     // Step 5 : 빈 문자열에 대해 a를 대입하기
-    if (tmp4.size() == 0)
-        tmp5 += 'a';
-    else
-        tmp5 = tmp4;
+    if (result.size() == 0) result += 'a';
     
     // Step 6 : 긴 문자를 제거하자
-    if (tmp5.size() >= 16)
+    if (result.size() >= 16)
     {
-        tmp6 = tmp5.substr(0, 15);
+        result = result.substr(0, 15);
         
-        if(tmp6.back() == '.')
-            tmp6 = tmp6.substr(0, 14);
-    }
-    else
-    {
-        tmp6 = tmp5;
+        if(result.back() == '.')
+            result.erase(result.end() - 1);
     }
     
     // Step 7 : 강제 늘리기
-    if (tmp6.size() <= 2)
+    if (result.size() <= 2)
     {
-        tmp7 = tmp6;
-        while(tmp7.size() != 3)
-        {
-            tmp7 += tmp7.back();
-        }
+        while(result.size() != 3)
+            result += result.back();
     }
-    else
-        tmp7 =tmp6;
     
-    return tmp7;
+    return result;
 }
