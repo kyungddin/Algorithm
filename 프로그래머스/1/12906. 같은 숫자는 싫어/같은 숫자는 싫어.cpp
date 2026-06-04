bb876e296de4b37189aaebfd548e9832b37a388b
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-06-04 20:21:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 83.79 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,26 +1,8 @@
 #include <vector>
-#include <stack>
 #include <algorithm>
-using namespace std;
-
-vector<int> solution(vector<int> arr) 
+std::vector<int> solution(std::vector<int> arr) 
 {
-    vector<int> answer;
-    stack<int> uniqueStack;
-    
-    for (auto num : arr)
-    {
-        if(uniqueStack.size() == 0) uniqueStack.push(num);
-        if(uniqueStack.top() != num) uniqueStack.push(num);
-    }
-    
-    int size = uniqueStack.size();
-    for (int i = 0; i < size; i++)
-    {
-        answer.push_back(uniqueStack.top());
-        uniqueStack.pop();
-    }
-    reverse(answer.begin(), answer.end());
-    
+    arr.erase(std::unique(arr.begin(), arr.end()),arr.end());
+    std::vector<int> answer =  arr;
     return answer;
 }
