e18db227baa710fe9bd1a1fd9306cafec1e575ae
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-06-05 23:23:20 +0900

    [level 2] Title: 주식가격, Time: 20.00 ms, Memory: 26.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,9 +1,36 @@
 #include <string>
 #include <vector>
+#include <stack>
+#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
+    stack<int> idxStack;
+    int size = prices.size();
+    int topIdx = 0;
+    
+    answer.resize(size);
+    idxStack.push(0);
+    
+    for(int idx=1; idx < size; idx++)
+    {
+        while(!idxStack.empty() && (prices[idxStack.top()] > prices[idx]))
+        {
+            topIdx = idxStack.top();
+            idxStack.pop();
+            answer[topIdx] = idx - topIdx;
+        }
+        idxStack.push(idx);
+    }
+    
+    const int MAX_VAL = size - 1;
+    while (idxStack.size())
+    {
+        answer[idxStack.top()] = MAX_VAL - idxStack.top();
+        idxStack.pop();
+    }
+    
     return answer;
 }
