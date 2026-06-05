f56ef04b4469dc0b93d72930b59335f1514ba2a2
Kyungmin Kim <kyungddin@cau.ac.kr>
2026-06-05 16:23:41 +0900

    [level 2] Title: 주식가격, Time: 20.93 ms, Memory: 26.7 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    int count = 0;
+    for (int i = 0; i <prices.size() - 1; i++)
+    {
+        for (int j = i + 1 ; j < prices.size(); j++)
+        {
+            count++; 
+            if (prices[j] < prices[i]) break;
+        }
+        
+        answer.push_back(count);
+        count = 0;
+    }
+    answer.push_back(0);
+    
+    return answer;
+}
